99c122038993976349c349e77e517105267fc540
Shrihari-Lute <luteshrihari@gmail.com>
2021-09-27 17:07:34 +0530

    Data Types

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-console.log('Hey this is my first piece of JS code!!! YAY')
-console.warn("This is warning")
-console.error("This is error")
+// Data Types
+// 1)Primitive Data Type
+// 2)Non-Primitive Data Type
+
+/* PRIMITIVE DATA TYPE */
+
+let age=20;
+console.log(age)
+console.log(typeof age)
+
+let Name="Shrihari"
+console.log(Name)
+console.log(typeof Name)
+
+let brand
+console.log(brand)
+
+
+/* NON-PRIMITIVE DATA TYPE */
+
+let arr=[1,2,3,4,5]
+console.log(arr)
+
+arr.push("Value")
+console.log(arr)
+console.log(typeof arr)
