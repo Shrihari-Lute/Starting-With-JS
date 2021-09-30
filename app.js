2521f1f1861b2d0e8eb25590733b5cfbb2c5c4cc
Shrihari-Lute <luteshrihari@gmail.com>
2021-09-30 11:56:49 +0530

    Logical Operators

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
-// Comparison Operators
+// Logical Operators
 
-const a = 21
-const b = 7
-const c =21
+const a = 2
+const b = 3
 
-console.log("a =",a)
+console.log('a =',a)
+console.log('b =',b)
+console.log('a==b',a==b)
+console.log('a!=b',a!=b)
 
-console.log("b =",b)
+console.log("Truth Table For AND")
+console.log('')
 
-console.log("c =",c)
-
-console.log("a > b",a>b)
-
-console.log("a < b",a<b)
-
-console.log("a == b",a==b)
-
-console.log("a == c",a==c)
-
-console.log("-----------------------------------------")
+console.log('false && false =',false && false)
+console.log('true && true =',true && true)
+console.log('true && false =',true && false)
+console.log('false && true =',false && true)
+console.log('')
 
 
-const d = 23
-const e = '23'
+console.log("Truth Table For OR")
+console.log('')
 
-console.log("d == e",d==e)      // compare the value
-console.log("d === e",d===e)      // compare the type
+console.log('false || false =',false || false)
+console.log('true || true =',true || true)
+console.log('true || false =',true || false)
+console.log('false || true =',false || true)
+console.log('')
+
+const c = 10
+const d = 50
+const e = 55
+
+console.log('c =',c)
+console.log('d =',d)
+console.log('e =',e)
+
+console.log('e > c && e < d',e > c && e < d)
