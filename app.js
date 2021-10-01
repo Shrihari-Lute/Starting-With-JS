3fbb5e60ebec90188477bac6b708e19fc4aee0bc
Shrihari-Lute <luteshrihari@gmail.com>
2021-10-01 20:28:16 +0530

    Node Methods

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-// const element = document.getElementById('selectme')
+const element = document.getElementById('selectme')
 
 // const element= document.getElementsByClassName('heading')
 
 // const element= document.getElementsByTagName('h2')
 
-const element= document.querySelector('.heading')
+// const element = document.querySelector('#selectme')
+// element.innerHTML = "This is from JS"
 
-console.log(element)
+// element.style.color = "cyan"
+// element.style.fontsize = "40px"
+// element.style = "display:none"
+
+// console.log(element.classList)
+// element.classList.add('five')
+// console.log(element.classList)
+// console.classList.remove('one')
+// console.log(element.classList)
+
+const value = element.classList.contains('one')
+
+console.log(value)
