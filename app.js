ff8046540805ff49a4b3cdc1926a975ee802b669
Shrihari-Lute <luteshrihari@gmail.com>
2021-10-02 12:25:49 +0530

    Intro To Event Listener

--- app.js
+++ app.js
@@ -1,22 +1,6 @@
-const element = document.getElementById('selectme')
+const button = document.querySelector('button')
+const element = document.querySelector('body')
 
-// const element= document.getElementsByClassName('heading')
-
-// const element= document.getElementsByTagName('h2')
-
-// const element = document.querySelector('#selectme')
-// element.innerHTML = "This is from JS"
-
-// element.style.color = "cyan"
-// element.style.fontsize = "40px"
-// element.style = "display:none"
-
-// console.log(element.classList)
-// element.classList.add('five')
-// console.log(element.classList)
-// console.classList.remove('one')
-// console.log(element.classList)
-
-const value = element.classList.contains('one')
-
-console.log(value)
+button.addEventListener('click', () => {
+    element.classList.toggle('dark')
+    })
